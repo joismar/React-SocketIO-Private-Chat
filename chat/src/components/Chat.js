ba03a47935e99c477ec6d18899a79caecf8f00d2
jennyffermorais <jennyffernovaes@hotmail.com>
2021-05-18 23:11:20 -0300

    Ajustar envio de mensagem

--- chat/src/components/Chat.js
+++ chat/src/components/Chat.js
@@ -1,97 +1,99 @@
-import React, { useState, useEffect } from 'react'
-import socket from "../socket"
-import MessagePanel from "./MessagePanel"
+import React, { useState, useEffect } from 'react';
+import socket from '../socket';
+import MessagePanel from './MessagePanel';
 // import User from "./User"
 
 function Chat(props) {
-	const [
-		userMessages,
-		setUserMessages,
-	] = useState([])
+   const [userMessages, setUserMessages] = useState([]);
 
-	const [
-		destUserData,
-		setDestUserData
-	] = useState({
-		userId: null,
-		connected: false,
-	})
+   const [destUserData, setDestUserData] = useState({});
 
-	useEffect(() => {
-		socket.on("private message", ({ content, from, to }) => {
-			onReceiveMessage(content, from)
-    })
-	}, [userMessages])
+   const [onReceived, setOnReceived] = useState(true);
 
-	useEffect(() => {
-		socket.on("connect", () => {
-			console.log(props.username)
-			socket.emit('user is online', props.destUsername)
-    })
+   useEffect(() => {
+      socket.on('private message', ({ content, from, to }) => {
+         if (onReceived) {
+            setOnReceived(false);
+            onReceiveMessage(content, from);
+         }
+      });
 
-		socket.on("user online", (userData) => {
-			setDestUserData(userData)
-		})
+      return () => {
+         setOnReceived(true);
+      };
+   }, [userMessages]);
 
-		return function cleanup() {
-      socket.off("connect")
-			socket.off("user online")
-			socket.off("private message")
-			socket.off("disconnect")
-    }
-	}, [props.destUsername])
+   useEffect(() => {
+      socket.on('connect', () => {
+         console.log(props.username);
+         socket.emit('user is online', props.destUsername);
+      });
 
-	useEffect(() => {
-		const timer = setInterval(() => {
-      socket.emit('user is online', props.destUsername)
-    }, 5000);
+      socket.on('user online', (userData) => {
+         setDestUserData(userData);
+      });
 
-		return function cleanup() {
-      clearInterval(timer)
-    }
-	}, [props.destUsername])
+      //return cleanup();
+   }, [props.destUsername]);
 
-	function onMessage(content, to) {
-		console.log(`Mensagem enviada para ${to}`)
-		socket.emit("private message", {
-			content,
-			to,
-		})
-		setUserMessages([
-			...userMessages,
-			{
-				content,
-				fromSelf: true,
-			}
-		])
-	}
+   function cleanup() {
+      socket.off('connect');
+      socket.off('user online');
+      socket.off('private message');
+      socket.off('disconnect');
+   }
 
-	function onReceiveMessage(content, from) {
-		console.log(`Mensagem ${content} recebida de ${from}`)
-		setUserMessages([
-			...userMessages,
-			{
-				content,
-				fromSelf: false,
-			}
-		])
-	}
+   useEffect(() => {
+      const timer = setInterval(() => {
+         socket.emit('user is online', props.destUsername);
+      }, 5000);
 
-	return (
-		<div>
-			<p>
-				UserId: {destUserData.userId}    
-				Username: {props.destUsername} {destUserData.connected ? '(ONLINE)' : '(OFFLINE)'}
-			</p>
-			<MessagePanel
-				username={props.username}
-				destUsername={props.destUsername}
-				destUserData={destUserData}
-				userMessages={userMessages}
-				onMessage={onMessage}
-			></MessagePanel>
-		</div>
-	)
+      return function cleanup() {
+         clearInterval(timer);
+      };
+   }, [props.destUsername]);
+
+   function onMessage(content, to) {
+      console.log(`Mensagem enviada para ${to}`);
+      socket.emit('private message', {
+         content,
+         to,
+      });
+      setUserMessages([
+         ...userMessages,
+         {
+            content,
+            fromSelf: true,
+         },
+      ]);
+   }
+
+   function onReceiveMessage(content, from) {
+      console.log(`Mensagem ${content} recebida de ${from}`);
+      setUserMessages([
+         ...userMessages,
+         {
+            content,
+            fromSelf: false,
+         },
+      ]);
+   }
+
+   return (
+      <div>
+         <p>
+            UserId: {destUserData.userID}     Username: {props.destUsername}{' '}
+            {destUserData.connected ? '(ONLINE)' : '(OFFLINE)'}
+         </p>
+         <MessagePanel
+            username={props.username}
+            destUsername={props.destUsername}
+            destUserData={destUserData}
+            userMessages={userMessages}
+            onMessage={onMessage}
+         ></MessagePanel>
+      </div>
+   );
 }
 
 export default Chat;
